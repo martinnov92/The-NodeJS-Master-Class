ee00cf66d5c89b6abd7c2cda2d0da23fc61b4f99
Martin Novák <martinnovak@outlook.com>
2018-06-25 08:59:14 +0200

    routing requests, returning json

--- MasterApp/index.js
+++ MasterApp/index.js
@@ -37,18 +37,37 @@ const server = http.createServer(function(req, res) {
     });
 
     // ukončení přenusu streamů
-    req.on('end', function(data) {
+    req.on('end', function() {
         // ukončení zápisu do bufferu
         // decoder.end() => z dokumentace NodeJS, je potřeba to zavolat, protože StringDecoder má svůj vnitřní buffer, ve kterém si drží zbývající input data
         buffer += decoder.end();
 
-        // až po ukončení přenusu dat odešlu odpověď a zaloguji to
-        // odeslat odpověď
-        res.end('Hello world.\n');
+        // zvolení handleru, který potřebuje tenhle request, pokud neexistuje => vrátit 404
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // zalogovat cestu
-        // console.log(`Requested path ${trimmedPath} by ${method} with headers ${JSON.stringify(headers, null, 4)} with query: ${JSON.stringify(queryStringObject, null, 4)}`);
-        console.log(`Request received with this payload: `, buffer);
+        // vytvoření data objectu pro předání do handleru
+        const data = {
+            method,
+            headers,
+            trimmedPath,
+            payload: buffer,
+            queryStringObject,
+        };
+
+        // route request pro daný handler přiřazený v handlers
+        chosenHandler(data, function(statusCode = 200, payload = {}) {
+            // convert payload na string
+            const payloadString = JSON.stringify(payload);
+
+            // nastavit do headru status code, content type, ukončit přenos a odeslat odpověď
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // zalogovat cestu
+            // console.log(`Requested path ${trimmedPath} by ${method} with headers ${JSON.stringify(headers, null, 4)} with query: ${JSON.stringify(queryStringObject, null, 4)}`);
+            console.log(`Returning this response: ${statusCode} (status code), ${payloadString}`);
+        });
     });
 });
 
@@ -57,3 +76,20 @@ server.listen(3000, function() {
     console.log('The server is listening on port 3000.');
 });
 
+// definování handlerů pro router
+const handlers = {};
+
+handlers.sample = function(data, callback) {
+    // callback http status code a payload (paylod === object)
+    callback(406, { 'name': 'sample handler' });
+};
+
+// NOT FOUND 404 handlers
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+// router
+const router = {
+    'sample': handlers.sample,
+};
