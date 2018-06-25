4bdf51e1e4a64a90c22dc24c9daa777421505630
Martin Novák <martinnovak@outlook.com>
2018-06-25 17:13:38 +0200

    Connecting to Twilio API

--- MasterApp/config.js
+++ MasterApp/config.js
@@ -10,6 +10,13 @@ environments.staging = {
     https: 3001,
     maxChecks: 5,
     envName: 'staging',
+    fromPhone: '',
+    twilio : {
+        // DEMO ÚDAJE Z KURZU
+        accountSid: 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        authToken: '9455e3eb3109edc12e3d8c92768f7a67',
+        fromPhone: '+15005550006'
+    },
     hashingSecret: 'martinkova skvela aplikace',
 };
 
@@ -19,6 +26,13 @@ environments.production = {
     https: 443,
     maxChecks: 5,
     envName: 'production',
+    fromPhone: '',
+    twilio : {
+        // DEMO ÚDAJE Z KURZU
+        accountSid: 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        authToken: '9455e3eb3109edc12e3d8c92768f7a67',
+        fromPhone: '+15005550006'
+    },
     hashingSecret: 'martinkova skvela aplikace',
 };
 
--- MasterApp/lib/helpers.js
+++ MasterApp/lib/helpers.js
@@ -1,4 +1,7 @@
+const https = require('https');
 const crypto = require('crypto');
+const queryString = require('querystring');
+
 const config = require('../config');
 
 // container
@@ -45,6 +48,62 @@ helpers.hash = (str) => {
     }
 
     return false;
+};
+
+// odesílání SMS pomocí Twilio
+helpers.sendTwilioSMS = (phone, msg, callback) => {
+    // validate parametry
+    phone = typeof phone === 'string' && phone.trim().length === 9 ? phone.trim() : false;
+    msg = typeof msg === 'string' && (msg.trim().length > 0 && msg.trim().length <= 160) ? msg : false;
+
+    if (phone && msg) {
+        // konfigurace requestu
+        const payload = {
+            From: config.twilio.fromPhone,
+            To: `+420${phone}`,
+            Body: msg,
+        };
+
+        // stringify
+        const stringPayload = queryString.stringify(payload);
+
+        // konfigurace request detailů
+        const requestDetails = {
+            protocol: 'https:',
+            hostname: 'api.twilio.com',
+            method: 'POST',
+            path: `/2010-04-01/Accounts/${config.twilio.accountSid}/Messages.json`,
+            auth: `${config.twilio.accountSid}:${config.twilio.authToken}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload), // získání délky bytů ze stringPayload
+            },
+        };
+
+        // vytvoření requestu a odeslat
+        const req = https.request(requestDetails, (res) => {
+            // získání statusu requestu
+            const status = res.statusCode;
+
+            // callback success
+            if (status === 200 || status === 201) {
+                callback(false);
+            }
+
+            callback(`Status code returned was ${status}`);
+        });
+
+        // bind to the error event, so it does not get thrown (aby nespadla aplikace)
+        req.on('error', callback);
+
+        // přidání payload do requestu
+        req.write(stringPayload);
+
+        // end request
+        req.end();
+    } else {
+        callback(400, { 'Error': 'Chybné nebo neúplné údaje.' });
+    }
 };
 
 module.exports = helpers;
