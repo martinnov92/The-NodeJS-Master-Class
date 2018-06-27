863bd53b67a8d806e58fb5130c77d45682e53504
Martin Novák <martinnovak@outlook.com>
2018-06-27 12:14:24 +0200

    cli manual

--- MasterApp/lib/cli.js
+++ MasterApp/lib/cli.js
@@ -10,6 +10,125 @@ const ev = new _events();
 // cli
 const cli = {};
 
+//#region EVENTS ON
+// input handlers
+ev.on('man', function(str) {
+    cli.responders.help();
+});
+
+ev.on('help', function(str) {
+    cli.responders.help();
+});
+
+ev.on('exit', function(str) {
+    cli.responders.exit();
+});
+
+ev.on('stats', function(str) {
+    cli.responders.stats();
+});
+
+ev.on('list users', function(str) {
+    cli.responders.listUsers();
+});
+
+ev.on('more user info', function(str) {
+    cli.responders.moreUserInfo(str);
+});
+
+ev.on('list checks', function(str) {
+    cli.responders.listChecks(str);
+});
+
+ev.on('more check info', function(str) {
+    cli.responders.moreCheckInfo(str);
+});
+
+ev.on('list logs', function(str) {
+    cli.responders.listLogs(str);
+});
+
+ev.on('more log info', function(str) {
+    cli.responders.moreLogInfo(str);
+});
+//#endregion
+
+//#region RESPONDERS
+cli.responders = {};
+
+cli.responders.help = function() {
+    const commands = {
+        'exit': 'Ukončí aplikaci',
+        'man': 'Zobrazí nápovědu',
+        'help': 'Zobrazí nápovědu',
+        'stats': 'Statistika operačního systému',
+        'list users': 'Seznam uživatelů',
+        'more user info --{userId}': 'Více informací k požadovanému uživateli',
+        'list checks --up / --down': 'Zobrazí seznam aktivinách záznamů, včetně jejich stavů. --up / --down jsou volitelné',
+        'more check info --{checkId}': 'Zobrazí detail požadovaného záznamu',
+        'list logs': 'Zobrazí seznam všech záznamů',
+        'more log info --{fileName}': 'Zobrazí detail záznamu',
+    };
+
+    // zobrazit hlavičku help page, která je široká jako okno
+    cli.horizontalLine();
+    cli.centered('CLI MANUAL');
+    cli.horizontalLine();
+    cli.verticalSpace(2);
+
+    // zobrazit jednotlivé příkazy
+    for (let command in commands) {
+        let value = commands[command];
+        let line = `\x1b[33m${command}\x1b[0m`;
+        let padding = 60 - command.length;
+
+        for (i = 0; i < padding; i++) {
+            line += ' ';
+        }
+
+        line += value;
+        console.log(line);
+        cli.verticalSpace();
+    }
+
+    cli.verticalSpace();
+    cli.horizontalLine();
+};
+
+cli.responders.exit = function() {
+    process.exit(0);
+};
+
+cli.responders.stats = function() {
+    console.log('STATS');
+};
+
+cli.responders.listUsers = function() {
+    console.log('LIST USERS');
+};
+
+cli.responders.moreUserInfo = function(str) {
+    console.log('LIST USERS', str);
+};
+
+cli.responders.listChecks = function(str) {
+    console.log('LIST CHECKS', str);
+};
+
+cli.responders.moreCheckInfo = function(str) {
+    console.log('MORE CHECK INFO', str);
+};
+
+cli.responders.listLogs = function() {
+    console.log('LIST LOGS');
+};
+
+cli.responders.moreLogInfo = function() {
+    console.log('MORE LOG INFO');
+};
+//#endregion
+
+//#region CLI methods
 cli.processInput = function(str) {
     str = (typeof str === 'string' && str.trim().length > 0) ? str : false;
 
@@ -78,5 +197,43 @@ cli.init = function() {
         process.exit(0);
     });
 };
+
+cli.horizontalLine = function() {
+    // získat velikost okna
+    const width = process.stdout.columns;
+    let line = '';
+
+    for (let i = 0; i < width; i++) {
+        line += '-';
+    }
+
+    console.log(line);
+};
+
+cli.centered = function(str) {
+    const width = process.stdout.columns;
+    str = typeof str === 'string' && str.trim().length > 0 ? str.trim() : '';
+
+    // vypočítat left padding
+    let leftPadding = Math.floor((width - str.length) / 2);
+
+    // přidat mezery
+    let line = '';
+
+    for (let i = 0; i < leftPadding; i++) {
+        line += ' ';
+    }
+
+    line += str;
+
+    console.log(line);
+};
+
+cli.verticalSpace = function(lines = 1) {
+    for (i = 0; i < lines; i++) {
+        console.log('');
+    }
+};
+//#endregion
 
 module.exports = cli;
